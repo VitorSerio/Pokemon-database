4bfec8c91c5e28fee1be6dab717fa8c85fae3c17
Vitor Barbosa Sério <vitorbserio@gmail.com>
2016-05-18 15:33:28 -0300

    Add files via upload

---
+++ pokemon.sql
@@ -0,0 +1,460 @@
+CREATE DATABASE pokemon;
+
+CREATE TABLE gen1
+	(
+	dexNum INTEGER(3) PRIMARY KEY AUTO_INCREMENT
+	name CHARACTER(15) PRIMARY KEY
+	);
+
+INSERT INTO gen1 (name)
+	VALUES ('Bulbasaur');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Ivysaur');
+
+INSERT INTO gen1 (name)
+	VALUES ('Venusaur');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Charmander');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Charmeleon');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Charizard');
+
+INSERT INTO gen1 (name)
+	VALUES ('Squirtle');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Wartortle');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Blastoise');
+
+INSERT INTO gen1 (name)
+	VALUES ('Caterpie');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Metapod');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Butterfree');
+
+INSERT INTO gen1 (name)
+	VALUES ('Weedle');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Kakuna');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Beedrill');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Pidgey');
+
+INSERT INTO gen1 (name)
+	VALUES ('Pidgeotto');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Pidgeot');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Rattata');
+
+INSERT INTO gen1 (name)
+	VALUES ('Raticate');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Spearow');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Fearow');
+
+INSERT INTO gen1 (name)
+	VALUES ('Ekans');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Arbok');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Pikachu');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Raichu');
+
+INSERT INTO gen1 (name)
+	VALUES ('Sandshrew');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Sandslash');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Nidoran♀');
+
+INSERT INTO gen1 (name)
+	VALUES ('Nidorina');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Nidoqueen');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Nidoran♂');
+
+INSERT INTO gen1 (name)
+	VALUES ('Nidorino');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Nidoking');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Clefairy');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Clefable');
+
+INSERT INTO gen1 (name)
+	VALUES ('Vulpix');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Ninetales');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Jigglypuff');
+
+INSERT INTO gen1 (name)
+	VALUES ('Wigglytuff');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Zubat');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Golbat');
+
+INSERT INTO gen1 (name)
+	VALUES ('Oddish');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Gloom');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Vileplume');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Paras');
+
+INSERT INTO gen1 (name)
+	VALUES ('Parasect');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Venonat');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Venomoth');
+
+INSERT INTO gen1 (name)
+	VALUES ('Diglett');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Dugtrio');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Meowth');
+
+INSERT INTO gen1 (name)
+	VALUES ('Persian');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Psyduck');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Golduck');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Mankey');
+
+INSERT INTO gen1 (name)
+	VALUES ('Primeape');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Growlithe');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Arcanine');
+
+INSERT INTO gen1 (name)
+	VALUES ('Poliwag');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Poliwhirl');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Poliwrath');
+
+INSERT INTO gen1 (name)
+	VALUES ('Abra');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Kadabra');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Alakazam');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Machop');
+
+INSERT INTO gen1 (name)
+	VALUES ('Machoke');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Machamp');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Bellsprout');
+
+INSERT INTO gen1 (name)
+	VALUES ('Weepinbell');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Victreebell');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Tentacool');
+
+INSERT INTO gen1 (name)
+	VALUES ('Tentacruel');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Geodude');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Graveler');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Golem');
+
+INSERT INTO gen1 (name)
+	VALUES ('Ponyta');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Rapidash');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Slowpoke');
+
+INSERT INTO gen1 (name)
+	VALUES ('Slowbro');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Magnemite');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Magneton');
+
+INSERT INTO gen1 (name)
+	VALUES ('Farfetch''s');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Doduo');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Dodrio');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Seel');
+
+INSERT INTO gen1 (name)
+	VALUES ('Dewgong');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Grimer');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Muk');
+
+INSERT INTO gen1 (name)
+	VALUES ('Shellder');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Cloyster');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Gastly');
+
+INSERT INTO gen1 (name)
+	VALUES ('Haunter');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Gengar');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Onix');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Drowzee');
+
+INSERT INTO gen1 (name)
+	VALUES ('Hypno');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Krabby');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Kingler');
+
+INSERT INTO gen1 (name)
+	VALUES ('Voltorb');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Electrode');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Exeggcute');
+
+INSERT INTO gen1 (name)
+	VALUES ('Exeggutor');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Cubone');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Marowak');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Hitmonlee');
+
+INSERT INTO gen1 (name)
+	VALUES ('Hitmonchan');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Lickitung');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Koffing');
+
+INSERT INTO gen1 (name)
+	VALUES ('Weezing');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Rhyhorn');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Rhydon');
+
+INSERT INTO gen1 (name)
+	VALUES ('Chansey');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Tangela');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Kangaskhan');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Horsea');
+
+INSERT INTO gen1 (name)
+	VALUES ('Seadra');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Goldeen');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Seaking');
+
+INSERT INTO gen1 (name)
+	VALUES ('Staryu');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Starmie');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Mr. Mime');
+
+INSERT INTO gen1 (name)
+	VALUES ('Scyther');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Jynx');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Elecabuzz');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Magmar');
+
+INSERT INTO gen1 (name)
+	VALUES ('Pinsir');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Tauros');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Magikarp');
+
+INSERT INTO gen1 (name)
+	VALUES ('Gyarados');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Lapras');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Ditto');
+
+INSERT INTO gen1 (name)
+	VALUES ('Eevee');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Vaporeon');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Jolteon');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Flareon');
+
+INSERT INTO gen1 (name)
+	VALUES ('Porygon');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Omanyte');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Omastar');
+
+INSERT INTO gen1 (name)
+	VALUES ('Kabuto');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Kabutops');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Aerodactyl');
+
+INSERT INTO gen1 (name)
+	VALUES ('Snorlax');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Articuno');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Zapdos');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Moltres');
+
+INSERT INTO gen1 (name)
+	VALUES ('Dratini');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Dragonair');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Dragonite');
+
+INSERT INTO gen1 (name)
+	VALUES ('Mewtwo');
+	
+INSERT INTO gen1 (name)
+	VALUES ('Mew');
